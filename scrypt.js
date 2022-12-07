ca1ca380eed6ab8b36ffef5b13a948d476aad8ee
Misha Voytenko <minyavoytenko@gmail.com>
2022-12-07 14:48:43 +0200

    end js

--- scrypt.js
+++ scrypt.js
@@ -1,23 +1,23 @@
-// let a = 0.1;
-// let b = 0.2;
-// // let sum = a + b;
-// // console.log(sum);
-
-// let x = "1";
-// let y = 2;
-// let sum = +x + 2;
+let a = 0.1;
+let b = 0.2;
+// let sum = a + b;
 // console.log(sum);
 
-// let size = prompt("вкажіть об'єм флешки у Гб");
-// let files = (size * 1024) / 820;
-// alert(files);
+let x = "1";
+let y = 2;
+let sum = +x + 2;
+console.log(sum);
 
-// let cash = prompt("введіть суму грошей");
-// let chocolets = prompt("введіть ціну однієї шоколадки");
-// let amount = Math.floor(cash / chocolets);
-// alert(amount);
-// let surrender = cash - chocolets * amount;
-// alert(surrender);
+let size = prompt("вкажіть об'єм флешки у Гб");
+let files = (size * 1024) / 820;
+alert(files);
+
+let cash = prompt("введіть суму грошей");
+let chocolets = prompt("введіть ціну однієї шоколадки");
+let amount = Math.floor(cash / chocolets);
+alert(amount);
+let surrender = cash - chocolets * amount;
+alert(surrender);
 
 let number = prompt("уведіть тризначне число");
 let firstResult = number % 10;
@@ -32,3 +32,10 @@ let thirdResult = thirdNumber % 10;
 console.log(thirdResult);
 let result = " " + firstResult + secondResult + thirdResult;
 alert(result);
+
+let i = 0;
+
+while (i < 5) {
+  console.log(i);
+  i++;
+}
